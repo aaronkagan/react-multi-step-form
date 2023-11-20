6546db323a39a1452f664927ef9195c60a9795ab
Aaron Kagan <aaronkagan@yahoo.com>
2023-11-20 08:57:15 -0500

    step 4 html

--- src/components/Step4.tsx
+++ src/components/Step4.tsx
@@ -2,6 +2,29 @@ const Step4 = () => {
   return (
     <article>
       <h1>Finishing up</h1>
+      <p>Double-check everything looks OK before confirming.</p>
+      <div>
+        <div>
+          <div>
+            <span>Arcade (Monthly)</span>
+            <button>Change</button>
+          </div>
+          <span>$9/mo</span>
+        </div>
+        <hr />
+        <div>
+          <span>Online service</span>
+          <span>+$1/mo</span>
+        </div>
+        <div>
+          <span>Larger storage</span>
+          <span>+$2/mo</span>
+        </div>
+      </div>
+      <div>
+        <span>Total (per month)</span>
+        <span>+$12/mo</span>
+      </div>
     </article>
   );
 };
