358744e528fd5f0dd7021ee7c7d8b548de832157
Aaron Kagan <aaronkagan@yahoo.com>
2023-11-20 10:12:32 -0500

    styling mobile navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,3 +1,5 @@
+import styled from 'styled-components';
+
 const Navigation = ({ step, setStep }) => {
   const handlePreviousStep = () => {
     if (step > 1) setStep((s) => s - 1);
@@ -8,15 +10,68 @@ const Navigation = ({ step, setStep }) => {
   };
 
   return (
-    <section>
-      <div>
-        {step !== 1 && step !== 5 && (
-          <button onClick={handlePreviousStep}>Go Back</button>
+    <StyledSection>
+      <div className="container">
+        {step !== 1 && step !== 5 ? (
+          <button
+            className="previous"
+            onClick={handlePreviousStep}
+          >
+            Go Back
+          </button>
+        ) : (
+          // Empty div for consistent positioning
+          <div></div>
         )}
-        {step !== 5 && <button onClick={handleNextStep}>Next Step</button>}
+        {step !== 5 && (
+          <button
+            className="next"
+            onClick={handleNextStep}
+          >
+            Next Step
+          </button>
+        )}
       </div>
-    </section>
+    </StyledSection>
   );
 };
+
+const StyledSection = styled.section`
+  font-size: 1.4rem;
+  font-weight: 500;
+  max-width: 42.5rem;
+  height: 7.2rem;
+  width: 100%;
+  background-color: #fff;
+  bottom: 0;
+  left: 50%;
+  position: fixed;
+  transform: translateX(-50%);
+
+  .container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 95%;
+    height: 100%;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  .previous {
+    color: #9699aa;
+    background: none;
+    border: none;
+  }
+
+  .next {
+    background-color: #022959;
+    border: none;
+    color: #fff;
+    padding: 1.2rem 1.6rem;
+    border-radius: 0.4rem;
+    cursor: pointer;
+  }
+`;
 
 export default Navigation;
