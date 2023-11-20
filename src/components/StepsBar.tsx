9bd13a2bf8b39884210ba8d185d3c8e1ef9636d0
Aaron Kagan <aaronkagan@yahoo.com>
2023-11-20 08:38:24 -0500

    changing logic for steps background colors

--- src/components/StepsBar.tsx
+++ src/components/StepsBar.tsx
@@ -2,21 +2,21 @@ const StepsBar = ({ step }) => {
   return (
     <aside>
       <div>
-        <span style={{ backgroundColor: step === 1 ? '#BEE2FD' : '' }}>1</span>
+        <span style={{ backgroundColor: step >= 1 ? '#BEE2FD' : '' }}>1</span>
         <div>
           <span>Step 1</span>
           <span>Your info</span>
         </div>
       </div>
       <div>
-        <span style={{ backgroundColor: step === 2 ? '#BEE2FD' : '' }}>2</span>
+        <span style={{ backgroundColor: step >= 2 ? '#BEE2FD' : '' }}>2</span>
         <div>
           <span>Step 2</span>
           <span>Select plan</span>
         </div>
       </div>
       <div>
-        <span style={{ backgroundColor: step === 3 ? '#BEE2FD' : '' }}>3</span>
+        <span style={{ backgroundColor: step >= 3 ? '#BEE2FD' : '' }}>3</span>
         <div>
           <span>Step 3</span>
           <span>Add ons</span>
