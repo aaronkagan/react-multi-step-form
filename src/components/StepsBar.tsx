c77086d44df4a8a4fd6ea1d17a483f489af40cfe
Aaron Kagan <aaronkagan@yahoo.com>
2023-11-20 09:28:13 -0500

    styling mobile sidebar

--- src/components/StepsBar.tsx
+++ src/components/StepsBar.tsx
@@ -4,28 +4,56 @@ const StepsBar = ({ step }) => {
   return (
     <StyledAside>
       <div>
-        <span style={{ backgroundColor: step >= 1 ? '#BEE2FD' : '' }}>1</span>
+        <span
+          className="steps-circles"
+          style={
+            step >= 1 ? { backgroundColor: '#BEE2FD', border: 'none' } : {}
+          }
+        >
+          1
+        </span>
         <div className="hidden-mobile ">
           <span>Step 1</span>
           <span>Your info</span>
         </div>
       </div>
       <div>
-        <span style={{ backgroundColor: step >= 2 ? '#BEE2FD' : '' }}>2</span>
+        <span
+          className="steps-circles"
+          style={
+            step >= 2 ? { backgroundColor: '#BEE2FD', border: 'none' } : {}
+          }
+        >
+          2
+        </span>
         <div className="hidden-mobile ">
           <span>Step 2</span>
           <span>Select plan</span>
         </div>
       </div>
       <div>
-        <span style={{ backgroundColor: step >= 3 ? '#BEE2FD' : '' }}>3</span>
+        <span
+          className="steps-circles"
+          style={
+            step >= 3 ? { backgroundColor: '#BEE2FD', border: 'none' } : {}
+          }
+        >
+          3
+        </span>
         <div className="hidden-mobile ">
           <span>Step 3</span>
           <span>Add ons</span>
         </div>
       </div>
       <div>
-        <span style={{ backgroundColor: step >= 4 ? '#BEE2FD' : '' }}>4</span>
+        <span
+          className="steps-circles"
+          style={
+            step >= 4 ? { backgroundColor: '#BEE2FD', border: 'none' } : {}
+          }
+        >
+          4
+        </span>
         <div className="hidden-mobile ">
           <span>Step 4</span>
           <span>Summary</span>
@@ -40,9 +68,23 @@ const StyledAside = styled.aside`
   background-size: cover;
   min-height: 17.2rem;
   display: flex;
+  justify-content: center;
+  gap: 1.6rem;
 
   .hidden-mobile {
     display: none;
+  }
+
+  .steps-circles {
+    width: 3.3rem;
+    height: 3.3rem;
+    border-radius: 50%;
+    font-size: 1.4rem;
+    font-weight: 700;
+    display: grid;
+    place-items: center;
+    margin-top: 3.7rem;
+    border: 1px solid #fff;
   }
 `;
 
