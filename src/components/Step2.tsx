cf5ebd447ddb1ae54a1c681209365a2e4352334b
Aaron Kagan <aaronkagan@yahoo.com>
2023-11-20 08:47:51 -0500

    step 2 html

--- src/components/Step2.tsx
+++ src/components/Step2.tsx
@@ -2,6 +2,28 @@ const Step2 = () => {
   return (
     <article>
       <h1>Select Your Plan</h1>
+      <p>You have the option of monthly or yearly billing.</p>
+      <div>
+        <article>
+          <h3>Arcade</h3>
+          <span>$9/mo</span>
+        </article>
+        <article>
+          <h3>Advanced</h3>
+          <span>$12/mo</span>
+        </article>
+        <article>
+          <h3>Pro</h3>
+          <span>$15/mo</span>
+        </article>
+      </div>
+      <div>
+        <span>Monthly</span>
+        <div>
+          <div>TOGGLE</div>
+        </div>
+        <span>Yearly</span>
+      </div>
     </article>
   );
 };
