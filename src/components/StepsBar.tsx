7392d429eaa326b549fd0c8598a8d004e9058012
Aaron Kagan <aaronkagan@yahoo.com>
2023-11-20 09:17:23 -0500

    adding hidden mobile class

--- src/components/StepsBar.tsx
+++ src/components/StepsBar.tsx
@@ -5,28 +5,28 @@ const StepsBar = ({ step }) => {
     <StyledAside>
       <div>
         <span style={{ backgroundColor: step >= 1 ? '#BEE2FD' : '' }}>1</span>
-        <div>
+        <div className="hidden-mobile ">
           <span>Step 1</span>
           <span>Your info</span>
         </div>
       </div>
       <div>
         <span style={{ backgroundColor: step >= 2 ? '#BEE2FD' : '' }}>2</span>
-        <div>
+        <div className="hidden-mobile ">
           <span>Step 2</span>
           <span>Select plan</span>
         </div>
       </div>
       <div>
         <span style={{ backgroundColor: step >= 3 ? '#BEE2FD' : '' }}>3</span>
-        <div>
+        <div className="hidden-mobile ">
           <span>Step 3</span>
           <span>Add ons</span>
         </div>
       </div>
       <div>
         <span style={{ backgroundColor: step >= 4 ? '#BEE2FD' : '' }}>4</span>
-        <div>
+        <div className="hidden-mobile ">
           <span>Step 4</span>
           <span>Summary</span>
         </div>
@@ -39,6 +39,11 @@ const StyledAside = styled.aside`
   background-image: url('/public/assets/images/bg-sidebar-mobile.svg');
   background-size: cover;
   min-height: 17.2rem;
+  display: flex;
+
+  .hidden-mobile {
+    display: none;
+  }
 `;
 
 export default StepsBar;
